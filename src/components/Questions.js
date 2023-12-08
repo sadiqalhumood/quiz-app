8c9f88d5f0f8ac107025a6d4a4c1e97f9333b347
Aidan <aidsc@bu.edu>
2023-12-08 15:28:38 -0500

    connecting questions to app

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,96 +1,54 @@
 import React from "react";
 import Card from "./Card";
-import './QuizStyles.css';
-const Question = () => {
+import "./QuizStyles.css";
+
+const Question = ({
+                      questionIndex,
+                      setQuestionIndex,
+                      questions,
+                      setShowQuestionsPage,
+                      setShowFinalPage,
+                      score,
+                      setScore,
+                  }) => {
+    const handleClick = (isCorrect) => {
+        if (questionIndex < 9) {
+            if (isCorrect) {
+                setScore((score) => score += 100);
+            }
+            setQuestionIndex((prevIndex) => prevIndex + 1);
+        }
+        else{
+            if(isCorrect) {
+                setScore((score) => score += 100);
+            }
+            setShowQuestionsPage(false);
+            setQuestionIndex(true);
+        }
+    };
+
     return (
         <Card>
-            <h1 className="question">Example question</h1>
-
+            <h1 className="question">{questions[questionIndex].questionText}</h1>
             <div className="answers">
-                <div className="answer">
-                    <p>Answer</p>
-                </div>
-                <div className="answer">
-                    <p>Answer</p>
-                </div>
-                <div className="answer">
-                    <p>Answer</p>
-                </div>
-                <div className="answer">
-                    <p>Answer</p>
-                </div>
+                {questions[questionIndex].answers.map((answer, index) => (
+                    <div
+                        className="answer"
+                        key={index}
+                        onClick={() => handleClick(answer.correctAnswer)}
+                    >
+                        <p>{answer.answerText}</p>
+                    </div>
+                ))}
             </div>
-
             <p className="score">
-                Score: <span>0</span>
+                Score: <span>{score}</span>
             </p>
-
             <p className="question_number">
-                Question <span>1</span>/10
+                Question <span>{questionIndex + 1}</span>/10
             </p>
         </Card>
     );
 };
 
 export default Question;
-
-
-
-
-/*import React, { useState } from 'react';
-import './QuizStyles.css';
-
-const QuizContainer = () => {
-    // example questions
-    const questions = [
-        {
-            id: 1,
-            questionText: 'What is the capital of France?',
-            options: ['New York', 'London', 'Paris', 'Tokyo'],
-            answer: 'Paris'
-        },
-        {
-            id: 2,
-            questionText: 'Who wrote Hamlet?',
-            options: ['Charles Dickens', 'William Shakespeare', 'Leo Tolstoy', 'Mark Twain'],
-            answer: 'William Shakespeare'
-        },
-        // ... add more questions as needed
-    ];
-
-    // state to track the current question
-    const [currentQuestion, setCurrentQuestion] = useState(0);
-
-    // function to handle answer submission
-    const handleSubmit = (option) => {
-        // logic to check the answer and move to the next question
-        if (option === questions[currentQuestion].answer) {
-            console.log('correct!');
-        } else {
-            console.log('incorrect!');
-        }
-
-        // move to the next question
-        if (currentQuestion < questions.length - 1) {
-            setCurrentQuestion(currentQuestion + 1);
-        } else {
-            // end of quiz logic
-            console.log('quiz finished');
-        }
-    };
-
-    return (
-        <div className="question">
-            <h2>{questions[currentQuestion].questionText}</h2>
-            <div className="answers">
-                {questions[currentQuestion].options.map((option, index) => (
-                    <button key={index} className="answer" onClick={() => handleSubmit(option)}>
-                        {option}
-                    </button>
-                ))}
-            </div>
-        </div>
-    );
-};
-
-export default QuizContainer;*/
