d388341c33b5cf120d6fccc6a5aa18b6030706c5
celine <cboudaie@gmail.com>
2023-12-07 19:32:27 -0500

    Changes to questions component to work with correct answers component

--- src/components/questions.js
+++ src/components/questions.js
@@ -1,57 +1,59 @@
 import React, { useState } from 'react';
+import CorrectAnswers from './CorrectAnswers';
 import './QuizStyles.css';
 
 const QuizContainer = () => {
-    // example questions
     const questions = [
         {
             id: 1,
             questionText: 'What is the capital of France?',
             options: ['New York', 'London', 'Paris', 'Tokyo'],
-            answer: 'Paris'
+            answer: 'Paris',
         },
         {
             id: 2,
             questionText: 'Who wrote Hamlet?',
             options: ['Charles Dickens', 'William Shakespeare', 'Leo Tolstoy', 'Mark Twain'],
-            answer: 'William Shakespeare'
+            answer: 'William Shakespeare',
         },
         // ... add more questions as needed
     ];
 
-    // state to track the current question
     const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [userAnswers, setUserAnswers] = useState([]);
+    const [quizFinished, setQuizFinished] = useState(false);
 
-    // function to handle answer submission
     const handleSubmit = (option) => {
-        // logic to check the answer and move to the next question
-        if (option === questions[currentQuestion].answer) {
-            console.log('correct!');
-        } else {
-            console.log('incorrect!');
-        }
+        const isCorrect = option === questions[currentQuestion].answer;
 
-        // move to the next question
+        setUserAnswers([...userAnswers, { questionId: questions[currentQuestion].id, selectedAnswer: option, isCorrect }]);
+
         if (currentQuestion < questions.length - 1) {
             setCurrentQuestion(currentQuestion + 1);
         } else {
-            // end of quiz logic
-            console.log('quiz finished');
+            setQuizFinished(true);
         }
     };
 
     return (
         <div className="question">
-            <h2>{questions[currentQuestion].questionText}</h2>
-            <div className="answers">
-                {questions[currentQuestion].options.map((option, index) => (
-                    <button key={index} className="answer" onClick={() => handleSubmit(option)}>
-                        {option}
-                    </button>
-                ))}
-            </div>
+            {!quizFinished ? (
+                <>
+                    <h2>{questions[currentQuestion].questionText}</h2>
+                    <div className="answers">
+                        {questions[currentQuestion].options.map((option, index) => (
+                            <button key={index} className="answer" onClick={() => handleSubmit(option)}>
+                                {option}
+                            </button>
+                        ))}
+                    </div>
+                </>
+            ) : (
+                <CorrectAnswers questions={questions} userAnswers={userAnswers} />
+            )}
         </div>
     );
 };
 
 export default QuizContainer;
+
