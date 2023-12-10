edd7def13290823b7bcfbc38871962b78411dc18
Aidan <aidsc@bu.edu>
2023-12-10 18:12:05 -0500

    added even more comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,59 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 import './App.css';
-import FinalPage from "./components/FinalPage";
 import StartingPage from "./components/StartingPage";
-import QuestionsPage from "./components/QuestionsPage";
-
-function App() {
-
+import Result from "./components/Result";
+import QuizContainer from "./components/Questions";
 
 
-  const [showStartingPage, setShowStartingPage] = useState(true);
-  const [showQuestionsPage, setShowQuestionsPage] = useState(false);
-  const [showFinalPage, setShowFinalPage] = useState(false);
+const App = () => {
+  /* State variables for managing quiz state */
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [quizFinished, setQuizFinished] = useState(false);
+  const [userScore, setUserScore] = useState(0);
 
-  const [score, setScore] = useState(0);
-  const [topScore, setTopScore] = useState(0);
+  /* Starting the quiz */
+  const handleQuizStart = () => {
+    setQuizStarted(true);
+  };
 
-  const [username, setUsername] = useState("");
+  /* Finishing the quiz and updating user score */
+  const handleQuizFinish = (score) => {
+    setQuizFinished(true);
+    setUserScore(score);
+  };
 
+  /* Restarting the quiz */
+  const handleRestartQuiz = () => {
+    setQuizStarted(false);
+    setQuizFinished(false);
+    setUserScore(0);
+  };
+
+  /* Render the App component */
   return (
-    <>
-      {showStartingPage && (
-        <StartingPage
-          setShowStartingPage={setShowStartingPage}
-          setShowQuestionsPage={setShowQuestionsPage}
-          topScore={topScore}
-          setUsername={setUsername}
-        />
-      )}
+      <div className="app">
+        <h1>CS392 Quiz App</h1>
 
-      {showQuestionsPage && (
-        <QuestionsPage
-          score={score}
-          setScore={setScore}
-          setShowQuestionsPage={setShowQuestionsPage}
-          setShowFinalPage={setShowFinalPage}
-        />
-      )}
+        {/* Show the starting page if the quiz hasn't started or finished */}
+        {!quizStarted && !quizFinished && (
+            <StartingPage onStartQuiz={handleQuizStart} />
+        )}
 
-      {showFinalPage && (
-        <FinalPage
-          score={score}
-          setScore={setScore}
-          setShowStartingPage={setShowStartingPage}
-          setShowFinalPage={setShowFinalPage}
-        />
-      )}
-    </>
+        {/* Show the quiz container if the quiz has started but not finished */}
+        {quizStarted && !quizFinished && (
+            <QuizContainer onQuizFinish={handleQuizFinish} />
+        )}
+
+        {/* Show the result and restart button if the quiz has finished */}
+        {quizFinished && (
+            <>
+              <Result score={userScore} totalQuestions={5} />
+              <button onClick={handleRestartQuiz}>Restart Quiz</button>
+            </>
+        )}
+      </div>
   );
+};
 
-}
-
-
+/* Export the App component as the default export */
 export default App;
